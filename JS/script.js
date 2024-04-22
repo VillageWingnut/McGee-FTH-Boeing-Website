4dbaee9dee647e0c39cca0b905ebc3c8ea6e0c9c
VillageWingnut <137304105+VillageWingnut@users.noreply.github.com>
2024-04-22 10:55:58 -0400

    Add files via upload

--- JS/script.js
+++ JS/script.js
@@ -1,13 +1,19 @@
-function display() { 
-    var checkRadio = document.querySelector(
-        'input[name="historyPoll"]:checked');
-     
-    if(checkRadio != null) {
-        document.getElementById("airbus", "mcDon", "lockMart", "panAm", "twa").innerHTML
-            = checkRadio.value
+function checkPoll() {
+    // Get all radio buttons with the name "historyPoll"
+    var radios = document.getElementsByName("historyPoll");
+    var formValid = false;
+
+    // Loop through each radio button
+    for (var i = 0; i < radios.length; i++) {
+        // If a radio button is checked, set formValid to true and break the loop
+        if (radios[i].checked) {
+            formValid = true;
+            break;
+        }
     }
-    else {
-        document.getElementById("disp").innerHTML
-            = "No option selected";
+
+    // If no radio button is checked, display an alert
+    if (!formValid) {
+        alert("Please select an option before submitting.");
     }
 }
